e924dccc55950be5be8dc9ec354e026678365ce6
Sander Pettersen <sanderpette7@gmail.com>
2021-03-18 22:30:03 +0100

    second

--- modelleringX/sander_oppgave_1.py
+++ modelleringX/sander_oppgave_1.py
@@ -1,16 +1,34 @@
+'''
+En gammel legende sier at oppfinneren av sjakkspillet solgte oppfinnelsen sin til landets hersker. 
+Som betaling ville han ha:
+1 riskorn for den første ruta på sjakkbrettet
+2 riskorn for den andre ruta
+4 riskorn for den tredje ruta
+8 riskorn for den fjerde ruta
+...også videre. 
+
+Et sjakkbrett har 8 rader og 8 linjer. 
+Lag et program som besvarer disse to oppgavene:
+
+a)Hvor mange riskorn bli det totalt?
+b)På hvilken rute passerer vi 1.000.000 riskorn?
+'''
+
+import numpy as np
+import math
+
+# Printer ut et sjakk brett i terminalen
+board = np.zeros((8,8))
 
 
-# a)
-
-solgt = int(input("Hvor mange ruter har du solgt? "))
-
-
-# vet jeg er på vilspor her. Gjør den når jeg kommer hjem for denne skal jo være enkel.
-for x in range(0, solgt):
-    print(solgt * x) 
+# Liste for rutene som er solgt
+solgt = [] 
 
 
-        
+def funk(x):
+    return x * x
 
+for x in range(1, 65):
+    print(funk(x))
 
-
+print(128 * 128)
